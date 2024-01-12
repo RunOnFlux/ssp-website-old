307d29b73379e71d54282ef0838a30982395cf90
Emmanuel Steve <emmanuelsteve43@gmail.com>
2024-01-12 13:01:25 +0100

    changed video tag

--- components/videoComponent/videoComponent.js
+++ components/videoComponent/videoComponent.js
@@ -1,24 +1,27 @@
 import React from "react";
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 export function VideoComponent(){
-    const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
-    let videosrc = "/images/ssp animation 4 for site (1).webm";
+    // const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+    // let videosrc = "/images/ssp animation 4 for site (1).webm";
 
     return(
         <div>
-             <ReactPlayer
+             {/* <ReactPlayer
                 width="100%"
                 height="auto"
                 url={videosrc}
                 controls={true}
                 autoplay={true}
-                // light is usefull incase of dark mode
+             
                 light={false}
-                // picture in picture
+             
                 pip={true}
                 style={{marginTop: "30px"}}
             />
-            <source src={videosrc} type="video/mp4" />
+            <source src={videosrc} type="video/mp4" /> */}
+              <video class="img-fluid" width="100%" height="auto" controls autoplay muted src="./images/ssp animation 4 for site (1).webm" style={{marginTop: "30px"}}/>
+                                      
+              
         </div>
        
     )
