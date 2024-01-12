ebdf76f19544466aad388e3c5b3d443a84925f67
Emmanuel Steve <emmanuelsteve43@gmail.com>
2024-01-12 17:21:53 +0100

    changed accordion icon

--- components/videoComponent/videoComponent.js
+++ components/videoComponent/videoComponent.js
@@ -2,25 +2,25 @@ import React from "react";
 import dynamic from 'next/dynamic'
 
 export function VideoComponent(){
-    // const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
-    // let videosrc = "/images/animation.webm";
+    const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+    let videosrc = "/images/animation.mp4";
 
     return(
         <div>
-             {/* <ReactPlayer
+             <ReactPlayer
                 width="100%"
                 height="auto"
                 url={videosrc}
                 controls={true}
-                autoplay={true}
+                autoPlay="true"
              
                 light={false}
              
                 pip={true}
                 style={{marginTop: "30px"}}
-            /> */}
-            {/* <source src={videosrc} type="video/mp4" /> */}
-              <video  width="100%" height="auto" controls autoPlay muted src="/images/animation.mp4" style={{marginTop: "30px"}}/>
+            />
+            <source src={videosrc} type="video/mp4" />
+              {/* <video  width="100%" height="auto" controls autoPlay muted src="/images/animation.mp4" style={{marginTop: "30px"}}/> */}
                                       
               
         </div>
