d871f8ba9bd7149fee65d1ac3d851afdaf6398ac
Emmanuel Steve <emmanuelsteve43@gmail.com>
2024-01-12 15:18:41 +0100

    changed controls

--- components/videoComponent/videoComponent.js
+++ components/videoComponent/videoComponent.js
@@ -1,26 +1,26 @@
 import React from "react";
-// import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
 
 export function VideoComponent(){
-    // const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
-    // let videosrc = "/images/ssp animation 4 for site (1).webm";
+    const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+    let videosrc = "/images/ssp animation 4 for site (1).webm";
 
     return(
         <div>
-             {/* <ReactPlayer
+             <ReactPlayer
                 width="100%"
                 height="auto"
                 url={videosrc}
                 controls={true}
-                autoplay={true}
+                // autoplay={true}
              
                 light={false}
              
                 pip={true}
                 style={{marginTop: "30px"}}
             />
-            <source src={videosrc} type="video/mp4" /> */}
-              <video  width="100%" height="auto" controls autoplay muted src="/images/animation.webm" style={{marginTop: "30px"}}/>
+            <source src={videosrc} type="video/mp4" />
+              {/* <video  width="100%" height="auto" controls autoplay muted src="/images/animation.webm" style={{marginTop: "30px"}}/> */}
                                       
               
         </div>
