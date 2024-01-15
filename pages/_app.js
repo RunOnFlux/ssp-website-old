fb568a72f96c1b30584a20ff69ae81685318a266
Emmanuel Steve <emmanuelsteve43@gmail.com>
2024-01-15 12:14:16 +0100

    changed next-config

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,21 @@
-
+import React from "react";
 import Layout from "../components/layout";
 import 'bootstrap/dist/css/bootstrap.css';
 import "../styles/globals.css";
-
+import { SSRProvider } from 'react-bootstrap';
+import { useEffect } from "react";
 export default function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        import ('bootstrap/dist/js/bootstrap.js')
+    }, []);
   return (
-    <>
-   
-       <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    </>
+    <React.Fragment>
+        <SSRProvider>
+                <Layout>
+            <Component {...pageProps} />
+            </Layout>
+        </SSRProvider>
+    </React.Fragment>
    
   )
 }
