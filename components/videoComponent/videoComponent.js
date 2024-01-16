2f6eba62bdca14a4e5176d50a2c99067c9567ce3
Emmanuel Steve <emmanuelsteve43@gmail.com>
2024-01-16 11:18:12 +0100

    downgraded react.js

--- components/videoComponent/videoComponent.js
+++ components/videoComponent/videoComponent.js
@@ -1,29 +1,29 @@
 import React from "react";
-// import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
 
 export function VideoComponent(){
-    // const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
-    // let videosrc = "/images/animation.mp4";
-
+    const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+    let videosrc = "/images/animation.mp4";
+// 
     return(
         <div>
-             {/* <ReactPlayer
+             <ReactPlayer
                 width="100%"
                 height="auto"
                 url={videosrc}
                 controls={true}
-                autoPlay="true"
+                autoplay={true}
              
                 light={false}
              
                 pip={true}
                 style={{marginTop: "30px"}}
             />
-            <source src={videosrc} type="video/mp4" /> */}
-              {/* <video  width="100%" height="auto" controls autoPlay muted src="/images/animation.mp4" style={{marginTop: "30px"}}/> */}
-              <video width="100%" height="auto" controls autoPlay style={{marginTop: "30px"}}>
+            {/* <source src={videosrc} type="video/mp4" />
+              <video  width="100%" height="auto" controls autoPlay muted src="/images/animation.mp4" style={{marginTop: "30px"}}/> */}
+              {/* <video width="100%" height="auto" controls autoPlay style={{marginTop: "30px"}}>
             <source src="/images/animation.mp4" type="video/mp4"/>
-            </video>                       
+            </video>                        */}
               
         </div>
        
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,28 @@
 import React from "react";
+import Head from 'next/head'
 import Layout from "../components/layout";
 import 'bootstrap/dist/css/bootstrap.css';
 import "../styles/globals.css";
-import { SSRProvider } from 'react-bootstrap';
+
 import { useEffect } from "react";
 export default function MyApp({ Component, pageProps }) {
     useEffect(() => {
         import ('bootstrap/dist/js/bootstrap.js')
     }, []);
   return (
-    <React.Fragment>
-        <SSRProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <React.Fragment>
+
                 <Layout>
             <Component {...pageProps} />
             </Layout>
-        </SSRProvider>
+
     </React.Fragment>
+   
+    </>
    
   )
 }
